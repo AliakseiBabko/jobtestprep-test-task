50593fcce43649b5b3378603c196b19f8ddcea35
Aliaksei Babko <aliaksei.babko@mercer.com>
2024-09-09 18:41:36 +0400

    fix successfull login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,6 +9,7 @@ test('Login with valid credentials', { tag: ['@ui'] }, async ({ page }) => {
   
   // log into the app
   await loginPage.login();
+  await page.waitForURL('**/login');
 
   // check that the login was successful
   await expect(page.locator('h1.post-title')).toContainText('Logged In Successfully');
@@ -22,6 +23,7 @@ test('Login with invalid username', { tag: ['@ui'] }, async ({ page }) => {
   
   // log into the app with incorrect user
   await loginPage.login('incorrectUser');
+  await page.waitForURL('https://practicetestautomation.com/logged-in-successfully');
 
   // check that the error message is displayed
   await expect(loginPage.errorMessage).toContainText('Your username is invalid!');
